e430c85c69629cf40c5b4b762c0ccb676b45bca7
Anjana Sofia Vakil <anjanavakil@gmail.com>
2018-08-02 20:49:40 -0700

    Add pure-or-not examples

---
+++ pure-functions/pureOrNot.js
@@ -0,0 +1,98 @@
+'use strict';
+
+///////////////////////////////////////
+// Are the following functions pure? //
+///////////////////////////////////////
+
+
+
+////////////////
+//// Part 1 ////
+////////////////
+
+const getDate = () => new Date().toDateString();
+
+const getWorkshopDate = () => new Date(2018, 7, 9).toDateString();
+
+
+
+////////////////
+//// Part 2 ////
+////////////////
+
+
+const jsonToObj = (filename) => {
+  const fs = require('fs');
+  const fileData = fs.readFileSync(filename, 'utf8');
+  return JSON.parse(fileData);
+}
+
+const objToJson = (object) => {
+  return JSON.stringify(object, null, 2);
+}
+
+
+
+////////////////
+//// Part 3 ////
+////////////////
+
+
+const logicalXor = (A, B) => (A || B) && !(A && B);
+
+const truthTable = (logicalOperator) => {
+  const values = [true, false];
+  const table = [];
+  for (const A of values) {
+    for (const B of values) {
+      table.push([A, B, logicalOperator(A, B)]);
+    }
+  }
+  return table;
+};
+
+const xorTruthTable = () => {
+  console.log('XOR truth table:\n================\n');
+  const table = truthTable(logicalXor);
+  console.log('\tA\t|\tB\t|\tA xor B');
+  console.log('\t-----\t|\t-----\t|\t-----');
+  table.map(([A, B, result]) => console.log(`\t${A}\t|\t${B}\t|\t${logicalXor(A,B)}`));
+};
+
+
+
+////////////////
+//// Part 4 ////
+////////////////
+
+// function examples in this section taken from
+// https://github.com/mapbox/mapbox-sdk-js/blob/master/lib/helpers/url-utils.js
+
+function encodeValue(value) {
+  if (Array.isArray(value)) {
+    return arrayValue.map(encodeURIComponent).join(',');
+  }
+  return encodeURIComponent(String(value));
+}
+
+/**
+ * Append a query parameter to a URL.
+ *
+ * @param {string} url
+ * @param {string} key
+ * @param {string|number|boolean|Array<*>>} [value] - Provide an array
+ *   if the value is a list and commas between values need to be
+ *   preserved, unencoded.
+ * @returns {string} - Modified URL.
+ */
+function appendQueryParam(url, key, value) {
+  if (value === false || value === null) {
+    return url;
+  }
+  var punctuation = /\?/.test(url) ? '&' : '?';
+  var query = encodeURIComponent(key);
+  if (value !== undefined && value !== '' && value !== true) {
+    query += '=' + encodeValue(value);
+  }
+  return '' + url + punctuation + query;
+}
