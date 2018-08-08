f50a20565eb618e95008a6bbb05cb4de136b4c9f
Anjana Sofia Vakil <anjanavakil@gmail.com>
2018-08-09 00:23:44 +0300

    updates

--- composition/snakeCharmer.js
+++ composition/snakeCharmer.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const pipeline = require('./pipeline.js');
-const { filter, map, reduce } = require('../recursion/filterMapReduce.js');
+const { count, head, tail } = require('../arrayHelpers.js');
+const { map, reduce, filter } = require('../recursion/filterMapReduce.js');
+
 
 const desnake = (snake_case_string) => snake_case_string.split('_');
-const capitalize = (string) => string.toUpper();
-const camelize = (stringArray) => {}
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
+const capitalizeAll = (stringArray) => map(capitalize, stringArray)
+const camelize = (stringArray) => [head(stringArray)].concat(capitalizeAll(tail(stringArray)));
+const concatenate = (stringArray) => reduce((acc, str) => acc + str, '', stringArray);
+const hyphenate = (stringArray) => reduce((acc, str) => [acc, str].join('-'), head(stringArray), tail(stringArray));
 
-const snakeToCamel = (snake_case_string) => {
-  // TODO return camelCaseString
-};
 
-const snakeToTrain = (snake_case_string) => {
-  // TODO return Train-Case-String
-};
+const snakeToCamel = pipeline(desnake, camelize, concatenate);
+const snakeToTrain = pipeline(desnake, capitalizeAll, hyphenate);
 
 
 module.exports = { snakeToCamel, snakeToTrain };
--- composition/snakeCharmer.test.js
+++ composition/snakeCharmer.test.js
@@ -5,11 +5,13 @@ const { snakeToCamel, snakeToTrain } = require('./snakeCharmer.js');
 
 test('[snakeCharmer] snakeToCamel', (assert) => {
   assert.equal(snakeToCamel('super_cool_name'), 'superCoolName');
+  assert.equal(snakeToCamel('functional_programming_rocks'), 'functionalProgrammingRocks');
   assert.end();
 });
 
 test('[snakeCharmer] snakeToTrain', (assert) => {
-  assert.equal(snakeToCamel('super_cool_name'), 'Super-Cool-Name');
+  assert.equal(snakeToTrain('super_cool_name'), 'Super-Cool-Name');
+  assert.equal(snakeToTrain('functional_programming_rocks'), 'Functional-Programming-Rocks');
   assert.end();
 });
 
