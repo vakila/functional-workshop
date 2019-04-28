9404bbbd25cb2be0b0dbd0adf657144f33fb7209
Anjana Vakil <anjana.vakil@mapbox.com>
2019-04-28 12:59:48 -0500

    Add fibonacci solutions

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -2,17 +2,39 @@
 
 
 const imperativeFibonacci = (n) => {
-  // TODO
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  
+  let previous = 0;
+  let current = 1;
+  for (let i = n; i > 1; i--) {
+    let next = previous + current;
+    previous = current;
+    current = next;
+  }
+  return current;
 
 };
 
 
 
 const recursiveFibonacci = (n) => {
-  // TODO
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return recursiveFibonacci(n-2) + recursiveFibonacci(n-1);
 };
 
 
 const tailRecursiveFibonacci = (n) => {
-  // TODO
-}
+  
+  const fib = (n, current, previous) => {
+    if (n === 0) return previous;
+    if (n === 1) return current; 
+    return fib(n-1, current + previous, current);
+  }
+  return fib(n, 1, 0); 
+};
+
+
+
+
